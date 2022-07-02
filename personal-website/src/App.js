b01e1a56eb1a383a07aa5cfbd31f14478eb39ce7
OverZone-Production <supervegito835@example.com>
2022-07-02 15:04:23 +0700

    Layout

---
+++ personal-website/src/App.js
@@ -0,0 +1,11 @@
+import LayoutDefault from "./layouts/LayoutDefault";
+
+const App = () => {
+  return (
+    <div className="app">
+      <LayoutDefault />
+    </div>
+  );
+}
+
+export default App;
---
+++ personal-website/src/layouts/LayoutDefault.js
@@ -0,0 +1,22 @@
+const LayoutDefault = () => {
+
+    return (
+        <div className="layout-default outline outline-2">
+            {/* Navigation Bar */}
+            <div 
+            className="navbar w-screen h-12"
+            >
+                Personal Website
+            </div>
+
+            <div
+                className="first-section w-52 h-32"
+            >
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default LayoutDefault
